56b4d927bd5e9135408d17c3a5b252a1e531a548
Maksim Vitman <mvitman@gamepoint.com>
2022-09-20 11:56:22 +0200

    fix collection iphone 10+ position coins and diam.

--- components/screens/collections/components/topPanel/TopPanelCollection.js
+++ components/screens/collections/components/topPanel/TopPanelCollection.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from "styled-components";
 import Coins from "../../../../common/TopPanel/components/Top/Coins";
 import Crystals from "../../../../common/TopPanel/components/Top/Crystals";
+import {NativeModules, Platform} from "react-native";
 
 const TopPanelCollection = (props) => {
     return (
@@ -17,9 +18,21 @@ const TopPanelCollection = (props) => {
 const TopPanel = styled.View`
     display: flex;
     align-items: center;
-    justify-content: space-around;
     width: 100%;
-    height: 50px;
+    ${()=>{
+    if(Platform.OS === 'ios' && NativeModules.DeviceInfo.isIPhoneX_deprecated){
+      return `
+         height: 80px;
+         justify-content: flex-end;
+         padding-bottom: 10px;
+      `
+    } else {
+      return `
+         height: 50px;
+         justify-content: space-around;
+      `
+    }
+    }}
     background-color: rgb(1,1,70);
 `
 const ElementsContainer = styled.View`
