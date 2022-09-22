1b9159b73e1d0a62908324dd11a7e78886c79590
Maksim Vitman <mvitman@gamepoint.com>
2022-09-22 11:14:29 +0200

    add dyn load squares

---
+++ assets/dynamicLoadGameSquares.js
@@ -0,0 +1,13 @@
+const imagesGameSquares = {
+    13: require('./collections/squares/1.png'),
+    5: require('./collections/squares/2.png'),
+    6: require('./collections/squares/3.png'),
+    7: require('./collections/squares/4.png'),
+    8: require('./collections/squares/5.png'),
+    9: require('./collections/squares/6.png'),
+    11: require('./collections/squares/7.png'),
+    10: require('./collections/squares/8.png'),
+    12: require('./collections/squares/9.png'),
+};
+
+export default imagesGameSquares;
